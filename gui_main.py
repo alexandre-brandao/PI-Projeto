a9d336c1dfb42bee25ba9aaeb56a8fc7c76de9a7
Rui Silva <rm.silva@outlook.pt>
2020-05-24 15:57:28 +0100

    Update ao GUI

--- gui_main.py
+++ gui_main.py
@@ -11,13 +11,58 @@ from tkinter import *
 
 #General Data
 
+def entrydetected():
+    maincanvas.itemconfigure(entryid, state='normal')
+    maincanvas.after(1500,maincanvas.itemconfigure(entryid, state='hidden'))
+
+def exitdetected():
+    maincanvas.itemconfigure(exitid, state='normal')
+    maincanvas.after(1500,lambda: maincanvas.itemconfigure(exitid, state='hidden'))
+
+def mainexec(Location,id,name):
+    Connected = 0
+    while Connected == 0:
+        try:
+            reader = SimpleMFRC522()
+            cnx, cursor = DBconnection()
+            Connected = 1
+        finally:
+            print("Connected\n")
+    try:
+        print(id)
+        if already_in(cursor, id, Location):
+            #Update data
+            update_location(cursor, "Raspberry Reader", id, "OUTSIDE")
+	    #Update history data related to the Prototype
+            add_to_history(cursor, id, "OUTSIDE")
+            print("OUTSIDE")
+            #exitdetected()
+            maincanvas.itemconfigure(exitid, state='normal')
+            maincanvas.after(1500,lambda: maincanvas.itemconfigure(exitid, state='hidden'))
+        else:
+            print(Location)
+            #Update data
+            update_location(cursor, "Raspberry Reader", id, Location)
+            #Update history data related to the Prototype
+            add_to_history(cursor, id, Location)
+            #entrydetected()
+            maincanvas.itemconfigure(entryid, state='normal')
+            maincanvas.after(1500,maincanvas.itemconfigure(entryid, state='hidden'))
+
+        cnx.commit()   # uploads data to the database
+
+    finally:
+        GPIO.cleanup()
+        cursor.close() # Closes the cursor(To be ignored for now)
+        name = '0'
+
 prototracker = tkinter.Tk()
 prototracker.attributes("-fullscreen",True)
 
 maincanvas = Canvas(prototracker, width = 1000, height = 1000)
-maincanvas.pack()
+
 logo = PhotoImage(file="altice_logo.png")
-maincanvas.create_image(175,250, image=logo)
+logoid = maincanvas.create_image(175,250, image=logo)
 
 entryimg = PhotoImage(file="entry.png")
 entryid = maincanvas.create_image(600,250, image=entryimg)
@@ -26,54 +71,30 @@ exitimg = PhotoImage(file="exit.png")
 exitid = maincanvas.create_image(600,250, image=exitimg)
 maincanvas.itemconfigure(exitid, state='hidden')
 
-def entrydetected():
-    maincanvas.itemconfigure(entryid, state='normal')
-    maincanvas.after(1500,lambda: maincanvas.itemconfigure(entryid, state='hidden'))
+maincanvas.itemconfigure(logoid, state='normal')
+maincanvas.pack()
 
-def exitdetected():
-    maincanvas.itemconfigure(exitid, state='normal')
-    maincanvas.after(1500,lambda: maincanvas.itemconfigure(exitid, state='hidden'))
-
-
-Connected = 0
 Location = "Building 3, Floor 1"
-
-while Connected == 0:
-	try:
-		reader = SimpleMFRC522()
-		cnx, cursor = DBconnection()
-		Connected = 1
-	finally:
-		print("Connected\n")
+reader = SimpleMFRC522()
+name = '0'
+interaction = 2
 		
 while 1:
-	try:
-		id,name = reader.read() # Wait for TAG
-		print(id)
-		
-		if already_in(cursor, id, Location):
-			#Update data
-			update_location(cursor, "Raspberry Reader", id, "OUTSIDE")
-			#Update history data related to the Prototype
-			add_to_history(cursor, id, "OUTSIDE")
-			print("OUTSIDE")
-			exitdetected()
-		else:
-			print(Location)
-			#Update data
-			update_location(cursor, "Raspberry Reader", id, Location)
-			#Update history data related to the Prototype
-			add_to_history(cursor, id, Location)
-			entrydetected()
-			
-			
-		cnx.commit()   # uploads data to the database
-		
-		
-	finally:
-		GPIO.cleanup()
+    #maincanvas.after(1500,lambda: maincanvas.itemconfigure(entryid, state='hidden'))
+    #maincanvas.after(1500,lambda: maincanvas.itemconfigure(exitid, state='hidden'))
+    print("Waiting for tag")
+    #id,name = reader.read()
+    time.sleep(10)
+    id = 54321
+    name = 'Test'
+    if name != '0':
+        mainexec(Location,id,name)
+        #prototracker.after(30000, mainexec(Location))
+        
+    prototracker.update_idletasks()
+    prototracker.update()
+    time.sleep(0.01)
 
-cursor.close() # Closes the cursor(To be ignored for now)
 
 
 
--- pi-rfid/read.py
+++ pi-rfid/read.py
@@ -5,8 +5,9 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-	id,text = reader.read()
-	print(id)
-	print(text)
+        print("running")
+        id,text = reader.read()
+        print(id)
+        print(text)
 finally:
 	GPIO.cleanup()
